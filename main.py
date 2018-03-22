f4774ce0a9bc959991d53792e9f0704da268ecf5
Gianmarco Calbi <calbigianmarco@gmail.com>
2018-03-22 09:36:18 +0100

    Sequential single-thread simulation for empty task now works

--- main.py
+++ main.py
@@ -3,46 +3,46 @@ from sklearn.preprocessing import normalize
 import time, random, math
 
 
+
 class Cluster:
-    def __init__(self, adjacency_matrix):
+    def __init__(self, setup):
         self.nodes = []
-        for i in range(adjacency_matrix.shape[1]):
+        self.log = []
+        self.settings = setup
+        for i in range(self.settings["adjacency_matrix"].shape[1]):
             self.nodes.append(Node(i))
-        for i in range(adjacency_matrix.shape[0]):
-            for j in range(adjacency_matrix.shape[1]):
-                if i != j and adjacency_matrix[i, j] == 1:
+            self.log.append([])
+        for i in range(self.settings["adjacency_matrix"].shape[0]):
+            for j in range(self.settings["adjacency_matrix"].shape[1]):
+                if i != j and self.settings["adjacency_matrix"][i, j] == 1:
                     self.nodes[j].add_dependency(self.nodes[i])
 
     def boot(self):
         pass
 
     def run(self):
-        ITERATION_AMOUNT = 100
         stop_condition = False
         while not stop_condition:
-            node = self.get_most_in_late_node()
-            if node.can_run():
-                node.step()
-            else:
-                max_local_clock = node.local_clock
-                for dep in node.dependencies:
-                    # restart from here:
-                    # control the following conditions
-                    if dep.iteration > node.iteration:
+            for node in self.get_most_in_late_nodes():
+                if node.can_run():
+                    self.log[node.id].append(node.step())
+                else:
+                    max_local_clock = node.local_clock
+                    for dep in node.dependencies:
                         if dep.get_local_clock_by_iteration(node.iteration) > max_local_clock:
-                            max_local_clock = dep.local_clock
-                node.set_local_clock(max_local_clock)
+                            max_local_clock = max(max_local_clock, dep.local_clock)
+                    node.set_local_clock(max_local_clock)
 
-            stop_condition = True
-            for node in self.nodes:
-                if node.iteration < ITERATION_AMOUNT:
-                    stop_condition = False
-                    break
+                stop_condition = True
+                for node in self.nodes:
+                    if node.iteration < self.settings["iteration_amount"]:
+                        stop_condition = False
+                        break
 
-            if node.id == 0 and node.iteration == 19:
-                print("stuck")
-                # input("press to continue....")
-                pass
+                if node.id == 0 and node.iteration == 19:
+                    print("stuck")
+                    # input("press to continue....")
+                    pass
 
     def get_most_in_late_node(self):
         candidate = None
@@ -52,6 +52,16 @@ class Cluster:
             elif node.local_clock < candidate.local_clock:
                 candidate = node
         return candidate
+
+    def get_most_in_late_nodes(self):
+        in_late_clock = self.get_most_in_late_node().local_clock
+        in_late_nodes = []
+        for node in self.nodes:
+            if node.local_clock == in_late_clock:
+                in_late_nodes.append(node)
+            elif node.local_clock < in_late_clock:
+                raise Exception("Node clock is less than the most in late clock. Impossible.")
+        return in_late_nodes
 
 
 class Node:
@@ -85,10 +95,13 @@ class Node:
         self.local_clock += t - t0
         self.iteration += 1
         """
-        self.local_clock += random.expovariate(0.5)
+        t0 = self.local_clock
+        tf = t0 + random.expovariate(0.5)
+        self.local_clock = tf
         self.iteration += 1
         self.log.append(self.local_clock)
         print("node ({0}) advanced to iteration #{1}".format(self.id, self.iteration))
+        return [t0, tf]
 
     def can_run(self):
         for dep in self.dependencies:
@@ -106,16 +119,18 @@ if __name__ == "__main__":
         [0, 0, 0, 1, 1, 1],
         [1, 0, 0, 0, 1, 1]
     ])
-    adjacency_matrix_zeros = np.zeros((6, 6))
     adjacency_matrix_diag = np.diag(np.ones(6))
     adjacency_matrix_bsp = np.ones((6, 6))
 
     markov_matrix = normalize(adjacency_matrix, axis=1, norm='l1')
 
-    nodes_amount = adjacency_matrix.shape[1]
-
     random.seed(2894)
 
-    cluster = Cluster(adjacency_matrix)
+    setup = {
+        "adjacency_matrix": adjacency_matrix,
+        "iteration_amount": 100
+    }
+
+    cluster = Cluster(setup)
     cluster.run()
     print(cluster.nodes[0].local_clock)
