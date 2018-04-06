Added sample generator, fixed error in GD computation (division by N was missing)

main.py
@@ -1,9 +1,10 @@
+import random, math
 import numpy as np
 from sklearn.datasets.samples_generator import make_blobs
 from sklearn.preprocessing import normalize
 
 from src.graph_generator import GraphGenerator
-from src.model import *
+from src import model, mltoolbox
 
 np.random.seed(2894)
 random.seed(2894)
@@ -12,10 +13,11 @@ if __name__ == "__main__":
     # adjacency_matrix = GraphGenerator.generate_d_regular_graph_by_edges(5, ["i->i+1"])
     __adjacency_matrix = GraphGenerator.generate_complete_graph(1)
     __markov_matrix = normalize(__adjacency_matrix, axis=1, norm='l1')
-    (__X, __y) = make_blobs(n_samples=10000, n_features=10, centers=2, cluster_std=2, random_state=20)
+    # (__X, __y) = make_blobs(n_samples=10000, n_features=10, centers=2, cluster_std=2, random_state=20)
+    (__X, __y) = mltoolbox.SampleGenerator.generate_linear_function_sample(10000, 10, 1)
 
     __setup = {
-        "iteration_amount": 10000,
+        "iteration_amount": math.inf,
     }
 
     __training_setup = {
@@ -23,9 +25,9 @@ if __name__ == "__main__":
         "y": __y,
         "alpha": 0.01,
         "activation_function": "sigmoid",
-        "method": "classic",  # stochastic
+        "method": "stochastic",  # classic or stochastic
         "batch_size": 1  # matters only for stochastic method
     }
 
-    __cluster = Cluster(__adjacency_matrix, __training_setup, __setup)
+    __cluster = model.Cluster(__adjacency_matrix, __training_setup, __setup)
     __cluster.run()

src/mltoolbox.py
@@ -1,11 +1,10 @@
 # import the necessary packages
-import matplotlib.pyplot as plt
-from sklearn.datasets.samples_generator import make_regression, make_blobs
 import numpy as np
-import random
-import time
+from sklearn.datasets.samples_generator import make_blobs
+import math
 
 np.random.seed(2894)
+
 
 class TrainingModel:
     def __init__(self, X, y, f, learning_rate, batch_size=1):
@@ -25,46 +24,48 @@ class TrainingModel:
         Gradient descent step function.
         :return: None
         """
+        N = self.X.shape[0]
+
         # get the prediction values by exploiting the sigmoid function
-        predictions = self.sigmoid(self.X.dot(self.W))
+        # predictions = self.sigmoid(self.X.dot(self.W))
+        predictions = self.X.dot(self.W)
 
         # compute the linear error as the difference between predicted y and y
         linear_error = predictions - self.y
 
-        # compute the loss function loss(f(X), y)
-        loss = np.sum(linear_error ** 2)
-        self.loss_log.append(loss)
+        # compute the error function (mean square error)
+        mean_square_error = np.sum(linear_error ** 2) / (2 * N)
+        self.loss_log.append(mean_square_error)
 
         # compute the gradient
-        gradient = self.X.T.dot(linear_error) / self.X.shape[0]
+        gradient = self.X.T.dot(linear_error) / N
 
         # update W following the steepest gradient descent
         self.W -= self.learning_rate * gradient
 
     def stochastic_gradient_descent_step(self):
         # determine the mini batch upon which compute the SGD
-        M = np.random.choice(self.X.shape[0], self.batch_size)
+        batch_indices = np.random.choice(self.X.shape[0], min(self.batch_size, self.X.shape[0]), replace=False)
+        M = batch_indices.shape[0]
 
-        sub_X = np.take(self.X, M, axis=0)
-        sub_y = np.take(self.y, M, axis=0)
+        sub_X = np.take(self.X, batch_indices, axis=0)
+        sub_y = np.take(self.y, batch_indices, axis=0)
 
         # get the prediction values by exploiting the sigmoid function
-        predictions = self.sigmoid(sub_X.dot(self.W))
+        predictions = sub_X.dot(self.W)
 
         # compute the linear error as the difference between predicted y and y
         linear_error = predictions - sub_y
 
         # compute the loss function loss(f(X), y)
-        loss = np.sum(linear_error ** 2)
-        self.loss_log.append(loss)
+        mean_square_error = np.sum(linear_error ** 2) / (2 * M)
+        self.loss_log.append(mean_square_error)
 
         # compute the gradient
-        gradient = sub_X.T.dot(linear_error) / sub_X.shape[0]
+        gradient = sub_X.T.dot(linear_error) / M
 
         # update W following the steepest gradient descent
         self.W -= self.learning_rate * gradient
-
-        pass
 
     @staticmethod
     def sigmoid(x):
@@ -76,13 +77,29 @@ class SampleGenerator:
         # a lot of useful parameters
         pass
 
-    def generate_sample(self):
-        return make_regression()
+    @staticmethod
+    def generate_linear_function_sample(n_samples, n_features, domain):
+        X = []
+        y = []
+        w = []
+        f = lambda _x, _w: _x.dot(_w)  # + np.random.rand()/domain
+
+        for _ in range(n_features):
+            w.append(np.random.uniform(-10, 10))
+
+        for i in range(n_samples):
+            x = np.random.uniform(-domain, domain, n_features)
+            X.append(x)
+            y.append(f(x, w))
+        return np.array(X), np.array(y)
 
 
 if __name__ == "__main__":
+    """
     (__X, __y) = make_blobs(n_samples=10, n_features=10, centers=2, cluster_std=2, random_state=20)
     lm = TrainingModel(__X, __y, lambda x: 2 * x, 0.005)
     while True:
         lm.stochastic_gradient_descent_step()
         print(lm.loss_log[-1])
+    """
+    print(SampleGenerator.generate_linear_function_sample(10, 1, 10))