539adaa837659d395db58ad812bc811b965a161d
Gianmarco Calbi <calbigianmarco@gmail.com>
2018-04-11 17:43:48 +0200

    Add plot of simple graph (error in iterations)

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ from sklearn.preprocessing import normalize
 from src.graph_generator import GraphGenerator
 from src import model, mltoolbox, console
 from curses import wrapper
+import matplotlib.pyplot as plt
 
 seed = 28041994
 np.random.seed(seed)
 random.seed(seed)
+
 
 def main(stdscr):
     console.stdout.screen = stdscr
@@ -18,11 +20,11 @@ def main(stdscr):
     __adjacency_matrix = GraphGenerator.generate_complete_graph(1)
     __markov_matrix = normalize(__adjacency_matrix, axis=1, norm='l1')
     # __X, __y = make_blobs(n_samples=10000, n_features=100, centers=3, cluster_std=2, random_state=20)
-    __X, __y = mltoolbox.SampleGenerator.sample_from_function(100000, 50, mltoolbox.linear_function, 10, biased=True)
+    __X, __y = mltoolbox.SampleGenerator.sample_from_function(1000, 50, mltoolbox.linear_function, 10, biased=True)
     # __X, __y = np.loadtxt("./dataset/largescale_challenge/alpha/alpha_train.dat"), np.loadtxt("./dataset/largescale_challenge/alpha/alpha_train.lab")
 
     __setup = {
-        "iteration_amount": math.inf
+        "iteration_amount": 1000
     }
 
     __training_setup = {
@@ -38,6 +40,17 @@ def main(stdscr):
     __cluster.run()
 
     console.stdout.close()
+
+    plt.xlabel("Iteration")
+    plt.ylabel("Mean Squared Error")
+    # plt.yscale('log')
+    # plt.axis(ymax=0.50)
+    # plt.annotate('Error {}'.format(__cluster.nodes[0].training_model.loss_log[-1]),
+    #   xy=(len(__cluster.nodes[0].training_model.loss_log)/2, 5))
+    plt.plot(list(range(0, len(__cluster.nodes[0].training_model.loss_log))),
+             __cluster.nodes[0].training_model.loss_log)
+    plt.show()
+
 
 if __name__ == "__main__":
     wrapper(main)
