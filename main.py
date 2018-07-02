Add 2nd eigenvalue computation, fix real score and start test suite n01

main.py
@@ -35,7 +35,7 @@ def main0(
     setup['n'] = 100 if n is None else n
 
     setup['graphs'] = graphs.generate_n_nodes_graphs(setup['n'], [
-        # "0_diagonal",
+        "0_diagonal",
         "1_cycle",
         "2_root_expander",
         # "2_uniform_edges",
@@ -47,7 +47,7 @@ def main0(
         # "8_cycle",
         "20_uniform_edges",
         # "20_cycle",
-        "50_uniform_edges",
+        #"50_uniform_edges",
         # "50_cycle",
         "n-1_clique",
     ])
@@ -57,19 +57,19 @@ def main0(
     setup['n_samples'] = 500 if n_samples is None else n_samples
     setup['n_features'] = 100 if n_features is None else n_features
 
-    setup['generator_function'] = 'reg'  # svm, reg, reg2, skreg
+    setup['generator_function'] = 'svm'  # svm, reg, reg2, skreg
 
-    setup['smv_label_flip_prob'] = 0.10  # <-- ONLY FOR SVM
+    setup['smv_label_flip_prob'] = 0.05  # <-- ONLY FOR SVM
 
     setup['error_mean'] = 0.0
-    setup['error_std_dev'] = 1.0  # <--
+    setup['error_std_dev'] = 0.0  # <--
 
     setup['node_error_mean'] = 0.0
     setup['node_error_std_dev'] = 0.0  # <--
 
     r = np.random.uniform(4, 10)
     c = np.random.uniform(1.1, 7.8) * np.random.choice([-1, 1, 1, 1])
-    setup['starting_weights_domain'] = [c - r, c + r]
+    setup['starting_weights_domain'] = [1, 2]# [c - r, c + r]
 
     # TRAINING SET ALMOST FIXED SETUP
     # SETUP USED ONLY BY REGRESSION 'reg':
@@ -77,22 +77,22 @@ def main0(
     setup['domain_center'] = 0
 
     # CLUSTER SETUP 1
-    setup['max_iter'] = 10
-    setup['max_time'] = None  # seconds
+    setup['max_iter'] = None
+    setup['max_time'] = 1000  # seconds
     setup['method'] = "classic"
     setup['dual_averaging_radius'] = 10
 
-    setup['alpha'] = 1e-4
+    setup['alpha'] = 1e-0
     setup['learning_rate'] = "root_decreasing"  # constant, root_decreasing
 
     setup['time_distr_class'] = statistics.ExponentialDistribution
     setup['time_distr_param'] = [1]  # [rate] for exponential, [alpha,sigma] for pareto, [a,b] for uniform
     setup['time_const_weight'] = 0 if time_const_weight is None else time_const_weight
 
-    setup['obj_function'] = 'mse'  # mse, hinge_loss, edgy_hinge_loss, score
+    setup['obj_function'] = 'hinge_loss'  # mse, hinge_loss, edgy_hinge_loss, score
 
-    setup['metrics'] = []
-    setup['real_metrics'] = []
+    setup['metrics'] = ['score']
+    setup['real_metrics'] = ['score']
     setup['metrics_type'] = 0  # 0: avg w on whole TS, 1: avg errors in nodes, 2: node's on whole TS
     setup['metrics_nodes'] = 'all'  # single node ID, list of IDs, otherwise all will be take into account in metrics
 
@@ -117,11 +117,10 @@ def main0(
             setup = pickle.load(setup_file)
 
     # OUTPUT SETUP
-    save_test_to_file = False  # write output files to "test_log/{test_log_sub_folder}/" folder
-    test_subfolder = "test_{}_exp1lambda_{}_err1_{}alpha_{}nodes{}samp{}feat_c{}_{}".format(
-        "x01",
+    save_test_to_file = True  # write output files to "test_log/{test_log_sub_folder}/" folder
+    test_subfolder = "test_{}_exp1lambda_{}_0.05flip_{}alpha_{}nodes{}samp{}feat_c{}_{}".format(
+        "n01",
         setup['generator_function'],
-        "err" + str(setup['error_std_dev']) if setup['error_std_dev'] != 0 else '',
         setup['learning_rate'][0] + str(setup['alpha']),
         setup['n'],
         setup['n_samples'],
@@ -290,6 +289,7 @@ Summary:
 
             cluster.setup(
                 X, y, w,
+                real_y_activation_function = np.sign,
                 obj_function=setup['obj_function'],
                 method=setup['method'],
                 max_iter=setup['max_iter'],

src/cluster.py
@@ -57,29 +57,30 @@ class Cluster:
         self.linear_regression_beta = None
 
     def setup(self, X, y, real_w,
-              obj_function=METRICS["mse"],
-              method="classic",
-              max_iter=None,
-              max_time=None,
-              batch_size=5,
-              dual_averaging_radius=10,
-              epsilon=0.0,
-              alpha=1e-4,
-              learning_rate="constant",
-              metrics="all",
-              real_metrics="all",
-              metrics_type=0,
-              metrics_nodes='all',
-              shuffle=True,
-              time_distr_class=statistics.ExponentialDistribution,
-              time_distr_param=(),
-              time_const_weight=0,
-              node_error_mean=0,
-              node_error_std_dev=1,
-              starting_weights_domain=(0, 5),
-              verbose_node=False,
-              verbose_task=False
-              ):
+            real_y_activation_function,
+            obj_function=METRICS["mse"],
+            method="classic",
+            max_iter=None,
+            max_time=None,
+            batch_size=5,
+            dual_averaging_radius=10,
+            epsilon=0.0,
+            alpha=1e-4,
+            learning_rate="constant",
+            metrics="all",
+            real_metrics="all",
+            metrics_type=0,
+            metrics_nodes='all',
+            shuffle=True,
+            time_distr_class=statistics.ExponentialDistribution,
+            time_distr_param=(),
+            time_const_weight=0,
+            node_error_mean=0,
+            node_error_std_dev=1,
+            starting_weights_domain=(0, 5),
+            verbose_node=False,
+            verbose_task=False
+    ):
         """Cluster setup.
 
         Parameters
@@ -169,8 +170,8 @@ class Cluster:
 
         """
 
-        self.verbose_node=verbose_node
-        self.verbose_task=verbose_task
+        self.verbose_node = verbose_node
+        self.verbose_task = verbose_task
         N = self.adjacency_matrix.shape[0]
 
         # if X and y have different sizes then the training set is bad formatted
@@ -266,7 +267,7 @@ class Cluster:
             self.y = np.take(Xy, -1, 1)
             del Xy
 
-        self.real_y = self.obj_function.y_hat_func.compute_value(self.X, self.real_w)
+        self.real_y = real_y_activation_function(self.obj_function.y_hat_func.compute_value(self.X, self.real_w))
 
         nodes_errors = np.random.normal(node_error_mean, node_error_std_dev, N)
         subX_size = int(math.floor(self.X.shape[0] / N))
@@ -698,7 +699,7 @@ class Cluster:
                     stop_condition = True
                     print(self._step_output())
                     print("Cluster stopped due to global clock (={}) being grater than or equal to max_time (={})".
-                          format(self.clock, self.max_time))
+                        format(self.clock, self.max_time))
                 elif self.get_obj_function_value() <= self.epsilon:
                     stop_condition = True
                     print(self._step_output())
@@ -867,7 +868,7 @@ class Cluster:
                     stop_condition = True
                     print(self._step_output())
                     print("Cluster stopped due to global clock (={}) being grater than or equal to max_time (={})".
-                          format(self.clock, self.max_time))
+                        format(self.clock, self.max_time))
                 elif self.get_obj_function_value() <= self.epsilon:
                     stop_condition = True
                     print(self._step_output())

src/functions.py
@@ -1,6 +1,7 @@
-import math, sys, random
+import math, sys, random, cmath
 import numpy as np
 from termcolor import colored as col
+
 
 def iteration_speed_lower_bound_new(distr_name, param):
     pass
@@ -29,11 +30,12 @@ def iteration_speed_lower_bound(l, k, time_arr):
         lb.append(t * l / (1 + sum(1 / i for i in range(1, k + 1))))
     return lb
 
+
 def compute_graph_degree_from_adjacency_matrix(adj_mat):
     degree = 0
 
     for i in range(len(adj_mat)):
-        degree = max(degree, np.sum(adj_mat[i])-1)
+        degree = max(degree, np.sum(adj_mat[i]) - 1)
 
     return int(degree)
 
@@ -41,18 +43,19 @@ def compute_graph_degree_from_adjacency_matrix(adj_mat):
 def compute_spectral_gap_from_adjacency_matrix(adj_mat):
     return 1 - compute_second_eigenvalue_from_adjacency_matrix(adj_mat)
 
+
 def compute_second_eigenvalue_from_adjacency_matrix(adj_mat):
     Ck = adj_mat[0]
     n = len(adj_mat)
     d = np.sum(Ck)
     max_val = 0
-    for m in range(1,n):
+    for m in range(1, n):
         s = 0.0
-        for k in range(0,n):
-            s += Ck[k] * math.e ** ()
-        s /= 1/(d+1)
-    return
-
+        for k in range(0, n):
+            s += Ck[k] * cmath.exp(-1j * 2 * math.pi * k * m / n)
+        s /= d + 1
+        max_val = max(s, max_val)
+    return max_val
 
 
 def progress(current_progress, total_progress, bar_length=50, text_before='', text_after=''):
@@ -65,6 +68,7 @@ def progress(current_progress, total_progress, bar_length=50, text_before='', te
 
     sys.stdout.write('{}[{}] {}% {}\r'.format(text_before, bar, percents, text_after))
     sys.stdout.flush()
+
 
 def print_verbose(level, msg, no_input=False):
     if level == 0:

src/mltoolbox/functions.py
@@ -148,7 +148,7 @@ def svm_dual_averaging_training_set(n_samples, n_features, label_flip_prob=0.05)
         x /= math.sqrt(np.inner(x, x))
         X.append(x)
         flip = 1
-        if np.random.uniform(0, 1) <= label_flip_prob:
+        if np.random.uniform(0, 1) < label_flip_prob:
             flip = -1
         y[i] = flip * np.sign(x.T.dot(w))
 