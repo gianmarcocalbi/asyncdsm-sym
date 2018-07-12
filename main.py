Add envelop script, Fix avg_w usage in gradient step

main.py
@@ -89,7 +89,7 @@ def main0(
         # "2-uniform_edges",
         "2-cycle",
         # "3-uniform_edges",
-        "3-cycle",
+        #"3-cycle",
         # "4-uniform_edges",
         "4-cycle",
         # "5-uniform_edges",
@@ -103,7 +103,7 @@ def main0(
         # "50-uniform_edges",
         "50-cycle",
         # "80-uniform_edges",
-        "80-cycle",
+        #"80-cycle",
         "99-clique",
     ])
 
@@ -124,7 +124,7 @@ def main0(
 
     r = np.random.uniform(4, 10)
     c = np.random.uniform(1.1, 7.8) * np.random.choice([-1, 1, 1, 1])
-    setup['starting_weights_domain'] = [-100, -100]  # [1, 2] #[c - r, c + r]
+    setup['starting_weights_domain'] = [-20, -20]  # [1, 2] #[c - r, c + r]
 
     # TRAINING SET ALMOST FIXED SETUP
     # SETUP USED ONLY BY REGRESSION 'reg':
@@ -132,13 +132,13 @@ def main0(
     setup['domain_center'] = 0
 
     # CLUSTER SETUP 1
-    setup['max_iter'] = 40
+    setup['max_iter'] = 100
     setup['max_time'] = None  # seconds
     setup['method'] = "classic"
     setup['dual_averaging_radius'] = 10
 
-    setup['alpha'] = 1e-3
-    setup['learning_rate'] = "root_decreasing"  # constant, root_decreasing
+    setup['alpha'] = 1e-2
+    setup['learning_rate'] = "constant"  # constant, root_decreasing
 
     setup['time_distr_class'] = statistics.ExponentialDistribution if time_distr_class is None else time_distr_class
     setup['time_distr_param'] = generate_time_distr_param_list(
@@ -153,9 +153,9 @@ def main0(
     setup['metrics'] = []
     setup['real_metrics'] = []
     setup['real_metrics_toggle'] = False  # False to disable real_metrics computation (to speed up computation)
-    setup['metrics_type'] = 2  # 0: avg w on whole TS, 1: avg errors in nodes, 2: node's on whole TS
-    setup['metrics_nodes'] = 'worst'  # single node ID, list of IDs, 'all', 'worst', 'best'
-    setup['shuffle'] = True  # <--
+    setup['metrics_type'] = 0  # 0: avg w on whole TS, 1: avg errors in nodes, 2: node's on whole TS
+    setup['metrics_nodes'] = 'all'  # single node ID, list of IDs, 'all', 'worst', 'best'
+    setup['shuffle'] = False  # <--
 
     # CLUSTER ALMOST FIXED SETUP
     setup['batch_size'] = 20
@@ -404,7 +404,7 @@ Summary:
 
         extension = '.txt'
         if compress:
-            extension = '.gz'
+            extension += '.gz'
 
         np.savetxt(
             os.path.join(test_path, "{}_iter_time_log{}".format(graph, extension)),
@@ -439,8 +439,8 @@ Summary:
                 delimiter=','
             )
 
-        # w_logs[graph] = cluster.w
-        # node_w_logs[graph] = cluster.nodes[0].training_task.w
+        w_logs[graph] = cluster.w
+        node_w_logs[graph] = cluster.nodes[0].training_task.w
 
         print("Logs of {} simulation created at {}".format(graph, test_path))
 
@@ -448,7 +448,7 @@ Summary:
         with open(os.path.join(test_path, '.descriptor.txt'), 'a') as f:
             f.write('\n\n# duration (hh:mm:ss): ' + time.strftime('%H:%M:%S', time.gmtime(time.time() - begin_time)))
 
-    """
+    #"""
     colors = Plotter.generate_color_dict_from_degrees(
         list(w_logs.keys()), setup['n']
     )
@@ -488,7 +488,7 @@ Summary:
     plt.legend()
     plt.show()
     plt.close()
-    """
+    #"""
 
     if save_plot_to_file or instant_plot:
         plot_from_files(

src/functions.py
@@ -1,6 +1,102 @@
-import math, sys, random, cmath
+import math, sys, random, cmath, pickle, os, warnings
 import numpy as np
 from termcolor import colored as col
+from src.mltoolbox.metrics import METRICS
+
+def load_test_logs(test_folder_path, return_setup=True):
+    try:
+        with open("{}/.setup.pkl".format(test_folder_path), 'rb') as setup_file:
+            setup = pickle.load(setup_file)
+    except:
+        print("No setup file to open")
+        raise
+
+    graphs = list(setup['graphs'].keys())
+    degrees = {}
+
+    for graph in graphs:
+        degrees[graph] = compute_graph_degree_from_adjacency_matrix(setup['graphs'][graph])
+
+    logs = {
+        "iter_time": {},
+        "avg_iter_time": {},
+        "max_iter_time": {},
+        "metrics": {}
+    }
+
+    # Fill metrics with instances of metrics objects
+    if not (isinstance(setup["metrics"], list) or isinstance(setup["metrics"], tuple)):
+        if setup["metrics"] in METRICS:
+            setup["metrics"] = [setup["metrics"]]
+        elif setup["metrics"].lower() == 'all':
+            setup["metrics"] = list(METRICS.keys())
+
+    if setup['real_metrics_toggle']:
+        if not (isinstance(setup["real_metrics"], list) or isinstance(setup["real_metrics"], tuple)):
+            if setup["real_metrics"] in METRICS:
+                setup["real_metrics"] = [setup["real_metrics"]]
+            elif setup["real_metrics"].lower() == 'all':
+                setup["real_metrics"] = list(METRICS.keys())
+
+    if not setup['obj_function'] in setup['metrics']:
+        setup['metrics'].insert(0, setup['obj_function'])
+
+    if setup['real_metrics_toggle'] and not setup['obj_function'] in setup['real_metrics']:
+        setup['real_metrics'].insert(0, setup['obj_function'])
+
+    for m in setup["metrics"]:
+        if m in METRICS:
+            logs["metrics"][m] = {}
+    if setup['real_metrics_toggle']:
+        for rm in setup["real_metrics"]:
+            if rm in METRICS:
+                logs["metrics"]["real_" + rm] = {}
+
+    # it's important to loop on a copy of graphs and not on the original one
+    # since the original in modified inside the loop
+    for graph in graphs[:]:
+        iter_log_path = "{}/{}_iter_time_log".format(test_folder_path, graph)
+        avg_iter_log_path = "{}/{}_avg_iter_time_log".format(test_folder_path, graph)
+        max_iter_log_path = "{}/{}_max_iter_time_log".format(test_folder_path, graph)
+
+        ext = ''
+        if not os.path.isfile(iter_log_path):
+            if os.path.isfile(iter_log_path + '.txt'):
+                ext = '.txt'
+            elif os.path.isfile(iter_log_path + '.gz'):
+                ext = '.gz'
+            elif os.path.isfile(iter_log_path + '.txt.gz'):
+                ext = '.txt.gz'
+            else:
+                raise Exception('File not found in {}'.format(test_folder_path))
+
+        iter_log_path += ext
+        avg_iter_log_path += ext
+        max_iter_log_path += ext
+
+        try:
+            logs["iter_time"][graph] = np.loadtxt(iter_log_path)
+            logs["avg_iter_time"][graph] = [tuple(s.split(",")) for s in np.loadtxt(avg_iter_log_path, str)]
+            logs["max_iter_time"][graph] = [tuple(s.split(",")) for s in np.loadtxt(max_iter_log_path, str)]
+        except OSError:
+            warnings.warn('Graph "{}" not found in folder {}'.format(graph, test_folder_path))
+            graphs.remove(graph)
+            continue
+
+        for metrics_log in logs["metrics"]:
+            metrics_log_path = "{}/{}_{}_log".format(test_folder_path, graph, metrics_log)
+            metrics_log_path += ext
+
+            try:
+                logs["metrics"][metrics_log][graph] = np.loadtxt(metrics_log_path)
+            except OSError:
+                warnings.warn('Graph "{}" not found in folder {}'.format(graph, test_folder_path))
+                graphs.remove(graph)
+                continue
+
+    if return_setup:
+        return logs, setup
+    return logs
 
 
 def iteration_speed_lower_bound_new(distr_name, param):

src/plotter.py
@@ -183,12 +183,20 @@ class Plotter:
             avg_iter_log_path = "{}/{}_avg_iter_time_log".format(self.test_folder_path, graph)
             max_iter_log_path = "{}/{}_max_iter_time_log".format(self.test_folder_path, graph)
 
-            if os.path.isfile(iter_log_path + ".gz"):
-                iter_log_path += '.gz'
-            if os.path.isfile(avg_iter_log_path + ".gz"):
-                avg_iter_log_path += '.gz'
-            if os.path.isfile(max_iter_log_path + ".gz"):
-                max_iter_log_path += '.gz'
+            ext = ''
+            if not os.path.isfile(iter_log_path):
+                if os.path.isfile(iter_log_path + '.txt'):
+                    ext = '.txt'
+                elif os.path.isfile(iter_log_path + '.gz'):
+                    ext = '.gz'
+                elif os.path.isfile(iter_log_path + '.txt.gz'):
+                    ext = '.txt.gz'
+                else:
+                    raise Exception('File not found in {}'.format(self.test_folder_path))
+
+            iter_log_path += ext
+            avg_iter_log_path += ext
+            max_iter_log_path += ext
 
             try:
                 self.logs["iter_time"][graph] = np.loadtxt(iter_log_path)
@@ -201,8 +209,7 @@ class Plotter:
 
             for metrics_log in self.logs["metrics"]:
                 metrics_log_path = "{}/{}_{}_log".format(self.test_folder_path, graph, metrics_log)
-                if os.path.isfile(metrics_log_path + ".gz"):
-                    metrics_log_path += '.gz'
+                metrics_log_path += ext
 
                 try:
                     self.logs["metrics"][metrics_log][graph] = np.loadtxt(metrics_log_path)

src/tasks.py
@@ -161,7 +161,7 @@ class GradientDescentTrainer(GradientDescentTrainerAbstract):
 
     def step(self, avg_w):
         # update W following the steepest gradient descent
-        gradient = self.obj_function.compute_gradient(self.X, self.y, avg_w)
+        gradient = self.obj_function.compute_gradient(self.X, self.y, self.get_w())
         self.w.append(avg_w - self.get_alpha() * gradient)
         self.iteration += 1
         self._compute_all_metrics()
@@ -175,7 +175,7 @@ class StochasticGradientDescentTrainer(GradientDescentTrainerAbstract):
         pick = np.random.randint(0, self.X.shape[0])
         X_pick = self.X[pick]
         y_pick = self.y[pick]
-        gradient = self.obj_function.compute_gradient(X_pick, y_pick, avg_w)
+        gradient = self.obj_function.compute_gradient(X_pick, y_pick, self.get_w())
         self.w.append(avg_w - self.get_alpha() * gradient)
         self.iteration += 1
         self._compute_all_metrics()
@@ -190,7 +190,7 @@ class BatchGradientDescentTrainer(GradientDescentTrainerAbstract):
                 "BatchGradientDescentTrainer started with batch_size = 1, it is preferable to use "
                 "StochasticGradientDescentTrainer instead")
 
-    def step(self):
+    def step(self, avg_w):
         # determine the mini batch upon which computing the SGD
         batch_indices = np.random.choice(self.X.shape[0], min(self.batch_size, self.X.shape[0]), replace=False)
 
@@ -198,7 +198,7 @@ class BatchGradientDescentTrainer(GradientDescentTrainerAbstract):
         X_batch = np.take(self.X, batch_indices, axis=0)
         y_batch = np.take(self.y, batch_indices, axis=0)
         gradient = self.obj_function.compute_gradient(X_batch, y_batch, self.get_w())
-        self.w.append(self.get_w() - self.get_alpha() * gradient)
+        self.w.append(avg_w - self.get_alpha() * gradient)
         self.iteration += 1
         self._compute_all_metrics()
 